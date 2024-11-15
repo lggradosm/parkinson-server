3ac1f460018e84122241e0e64ee6f6cdd7539f84
lggradosm <gabrielgradosmachado@gmail.com>
2024-11-15 13:43:13 -0500

    first commit

--- app/config/database.py
+++ app/config/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://root:password@postgres:5432/parkinson_db"
-SQLALCHEMY_DATABASE_URL = "postgresql://root:password@localhost:5432/parkinson_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://root:password@postgres:5432/parkinson_db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://root:password@localhost:5432/parkinson_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
