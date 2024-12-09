3c1f52c5ce17c081dc509c7ff6d6c66da577e554
lggradosm <gabrielgradosmachado@gmail.com>
2024-12-08 22:51:47 -0500

    first commit

--- app/routers/usuario_router.py
+++ app/routers/usuario_router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException,Request
+from fastapi import APIRouter, Depends, HTTPException,Request, status
 from schemas.UsuarioBase import UsuarioBase, LoginRequest
 from schemas.ResponseBase import ResponseBase
 from sqlalchemy.ext.asyncio import AsyncSession
 from config.database import get_db
 from services.usuario_service import create_usuario, login
+from datetime import datetime
 from datetime import date
 
 router = APIRouter()
@@ -11,17 +12,20 @@ router = APIRouter()
 async def registrar_usuario(usuario: Request, db:AsyncSession = Depends(get_db)):
   usuario_data = await usuario.json()
   fecha_nacimiento =  usuario_data.get("fecha_nacimiento")
+  fecha_datetime = datetime.strptime(fecha_nacimiento[:-1], "%Y-%m-%dT%H:%M:%S.%f")
   usuario_model = UsuarioBase(
-      nombres=usuario_data.get("nombres"),
-      apellidos=usuario_data.get("apellidos"),
-      contrasena=usuario_data.get("contrasena"),
-      nombre_usuario=usuario_data.get("nombreUsuario"),
-      genero=usuario_data.get("genero"),
-      fecha_nacimiento=date.today(),
-      estado=1
-      )
+  nombres=usuario_data.get("nombres"),
+  apellidos=usuario_data.get("apellidos"),
+  contrasena=usuario_data.get("contrasena"),
+  nombre_usuario=usuario_data.get("nombreUsuario"),
+  genero=usuario_data.get("genero"),
+  fecha_nacimiento=fecha_datetime.date(),
+  estado=1
+  )
   nuevo_usuario = await create_usuario(usuario_model, db)
   return nuevo_usuario
+  
+  
   
 @router.post("/login", response_model=ResponseBase)
 async def registrar_usuario(usuario: Request, db:AsyncSession = Depends(get_db)):
